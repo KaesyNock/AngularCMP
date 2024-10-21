b431721ab5fdfa715d309a770e47885dddf7b12b
KaesyNock <kaesynock@gmail.com>
2024-10-21 18:08:34 -0400

    styling and host element

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]', //see notes.txt
+  selector: 'button[appButton], a[appButton]', //see notes.txt
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
