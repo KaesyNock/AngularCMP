6e1ce49e48fb37314ab0d4f6e791d5406653d8a9
KaesyNock <kaesynock@gmail.com>
2024-10-22 14:28:43 -0400

    class binding

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, inject, input, ViewEncapsulation, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,12 +8,35 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+     '(click)': 'onClick()'
   }
+  //preferred method of adding :host
+  //Angular team prefer event listeners over @hostlistener
   //see notes.txt
 })
 export class ControlComponent {
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
+  //@HostListener allows you to bind a method to an event
+  // preferred approach for certain devs
+  // @HostBinding('class') className = 'control';<< exists for backward compatibility
+  //HostBinding looks at property className and sets control as the value
+  // () in hostbinding allows you to set the property enabling you to adjust the name of the property
+  // I.E. @HostBinding('class') className = 'control' (class) establishes the property, className allows you to avoid confusion with class property
+  //not the preferred method
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  // By using inject(ElementRef) angular gives you access to the host element of a component
+  //ElementRef refers to any Element on the page, obvs
+  //ElementRef allows programmatic access to component
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el)
+  }
 }
 
 
